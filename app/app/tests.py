4e14575fb4b8cf71b1743b4102b7a6dca3448088
Fernando Battisti da Silva <febattisti@outlook.com>
2025-10-27 17:15:53 -0300

    Added new features.

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+"""
+Sample Tests
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    """Test the calc model."""
+
+    def test_add_numbers(self):
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
